d7afe7ad3bf18e56c002be2c067185cba4415aca
Hayato Anzawa <anzawa@sb.ecei.tohoku.ac.jp>
2016-10-27 17:30:12 +0900

    parse qstat -j info

--- cqstat/qcommand.py
+++ cqstat/qcommand.py
@@ -4,6 +4,7 @@ from xml.etree import ElementTree
 from cluster import Cluster
 from queue import Queue
 from job import Job
+from qstatxml import parse_djob_info
 
 TAG2KWARG = dict(
     JB_job_number="job_ID", JAT_prio="prior", JAT_ntix="ntckts", JB_nppri="nprior",
@@ -14,8 +15,8 @@ TAG2KWARG = dict(
     cpu_usage="cpu", mem_usage="mem", io_usage="io", tickets="tckts",
     JB_override_tickets="ovrts", JB_jobshare="jobshare", otickets="otckt",
     ftickets="ftckt", stickets="stckt", JAT_share="share", queue_name="queue",
-    master="master_q", slots="slots", tasks="ja_task_id", hard_req_queue="h_resources",
-    soft_req_queue="s_resources", master_hard_req_queue="master_h_res"
+    master="master_q", slots="slots", tasks="ja_task_id", jclass_name="jclass",
+    hard_req_queue="h_resources", soft_req_queue="s_resources", master_hard_req_queue="master_h_res"
 )
 # predecessor_jobs_req, predecessor_jobs, requested_pe, granted_pe, JB_checkpoint_name. hard_request, def_hard_request, soft_request
 
@@ -37,6 +38,37 @@ def get_reduced_info(options):
     pending_jobs = _parse_jobs(root.findall("job_info/job_list"))
 
     return running_jobs, pending_jobs
+
+
+def get_jvi_info(joblist):
+    command = "qstat -xml -j "
+    if joblist:
+        command += joblist
+    else:
+        command += '*'
+
+    output = subprocess.check_output(command.split())
+    return parse_djob_info(output)
+
+
+def add_jvi_info(jobs, joblist=None):
+    if joblist is None:
+        joblist = ','.join(j.id.value for j in jobs)
+    attrs = get_jvi_info(joblist)
+
+    for j in jobs:
+        jid = j.id.value
+        task_id = j.ja_task_id.value
+
+        for k, v in attrs[jid].items():
+            if k in Job.attributes:
+                setattr(j, k, v)
+            elif k == "tasks" and task_id in v:
+                for uaname, value in v[task_id].items():
+                    setattr(j, uaname, value)
+            elif k == "jh_resources":
+                for rname, fvalue in v.items():
+                    setattr(j, rname, fvalue)
 
 
 def build_cluster(options, users):
---
+++ cqstat/qstatxml.py
@@ -0,0 +1,63 @@
+from xml.etree import ElementTree
+
+TAG2KWARG = dict(
+    JB_job_number="job_ID", JAT_prio="prior", JAT_ntix="ntckts", JB_nppri="nprior",
+    JB_nurg="nurg", JB_urg="urg", JB_rrcontr="rrcontr", JB_wtcontr="wtcontr",
+    JB_dlcontr="dlcontr", JB_priority="ppri", JB_name="name", JB_owner="user",
+    JB_project="project", JB_department="department", state="state",
+    JB_submission_time="sub_at", JAT_start_time="strt_at", JB_deadline="deadline",
+    cpu_usage="cpu", mem_usage="mem", io_usage="io", tickets="tckts",
+    JB_override_tickets="ovrts", JB_jobshare="jobshare", otickets="otckt",
+    ftickets="ftckt", stickets="stckt", JAT_share="share", queue_name="queue",
+    master="master_q", slots="slots", tasks="ja_task_id", jclass_name="jclass",
+    hard_req_queue="h_resources", soft_req_queue="s_resources", master_hard_req_queue="master_h_res"
+)
+# predecessor_jobs_req, predecessor_jobs, requested_pe, granted_pe, JB_checkpoint_name. hard_request, def_hard_request, soft_request
+
+VJI2KWARG = dict(
+    JB_job_number="job_ID", JB_job_name="name", JB_owner="user", JB_uid="uid",
+    JB_group="group", JB_gid="gid", JB_department="department",
+    JB_submission_time="sub_at", JAT_start_time="strt_at",
+    JB_jobshare="jobshare"
+)
+
+
+def _elems2list(tree, tag, ename="element"):
+    if tree is not None:
+        return [target.text for target in tree.findall(ename + "/" + tag)]
+    else:
+        return []
+
+
+def _elems2dict(tree, key, val, ename="element"):
+    if tree is not None:
+        return {elem.find(key).text: elem.find(val).text for elem in tree.findall(ename)}
+    else:
+        return {}
+
+
+def parse_djob_info(string):
+    root = ElementTree.fromstring(string)
+    jobs = {}
+
+    for job in root.findall("djob_info/element"):
+        attrs = {VJI2KWARG[e.tag]: e.text for e in job if e.tag in VJI2KWARG}
+        attrs["sup_group"] = ','.join(_elems2list(job.find("JB_supplementary_group_list"), "ST_name"))
+        attrs["jh_resources"] = _elems2dict(job.find("JB_hard_resource_list"), "CE_name", "CE_doubleval")
+
+        ja_tasks = {}
+        for task in job.findall("JB_ja_tasks/element"):
+            task_id = task.find("JAT_task_number").text
+            scaled_usage = _elems2dict(task.find("JAT_scaled_usage_list"), "UA_name", "UA_value", "Events")
+            ja_tasks[task_id] = scaled_usage
+        attrs["tasks"] = ja_tasks
+
+        jobs[attrs["job_ID"]] = attrs
+
+    return jobs
+
+
+if __name__ == "__main__":
+    import sys
+    with open(sys.argv[1]) as f:
+        print(parse_djob_info(''.join(f.readlines())))
